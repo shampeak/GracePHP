96ae649b37c7daf8e9ddf03fdcfed2ec92dc7007
shampeak <976107540@qq.com>
2016-06-06 17:40:50 +0800

    0.0008

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,10 +23,6 @@ class ComposerAutoloaderInited60b54d540461832eb71cf74d2cc90c
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInited60b54d540461832eb71cf74d2cc90c', 'loadClassLoader'));
 
-        $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
-        set_include_path(join(PATH_SEPARATOR, $includePaths));
-
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
